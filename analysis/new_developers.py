bd4cfb299cd6267522e2cdb6f1b3457afb336eb1
Daniel <danielgtravels@gmail.com>
2025-05-05 09:11:32 -0400

    fixed category sorter plotting

--- analysis/new_developers.py
+++ analysis/new_developers.py
@@ -10,25 +10,26 @@ index = 0
 for mcp in data:
     records.append({
         "MCP_index": index,
-        "published_date": mcp.get("published_date")
+        "published_date": mcp.get("published_date"),
+        "publisher": mcp.get("publisher")
     })
     index += 1
     
 df = pd.DataFrame(records)
 df['published_date'] = pd.to_datetime(df['published_date'], errors='coerce')
 df['week'] = df['published_date'].dt.to_period('W')
-df = df[['MCP_index', 'week']]
-df = df.groupby("MCP_index").first()
+df = df[['MCP_index', 'week', "publisher"]]
+df = df.groupby("publisher").first()
 trend = df['week'].value_counts().sort_index()
 
 plt.figure(figsize=(12, 5))
 trend.plot(marker='o')
-plt.title("MCP Servers Created by Week")
+plt.title("New MCP Developers by Week")
 plt.xlabel("Week")
-plt.ylabel("Total Servers")
+plt.ylabel("Number of New Developers")
 plt.grid(True)
 plt.tight_layout()
-plt.savefig("analysis/plots/servers_created_weekly.png")
+plt.savefig("analysis/plots/New_MCP_Developers_by_Week.png")
 plt.show()
 print(trend)
 
